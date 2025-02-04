aa3b89415c5b1994ead60d1ad73aa44546ca59d4
archananp1991 <archananp1991@gmail.com>
2025-02-04 11:23:02 -0500

    final task

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,8 +6,12 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
   
+   if response.status_code == 400:
+      return {"anger":None, "disgust":None, "fear":None, "joy":None, "sadness":None,"dominant_emotion":None}
+
    formatted_response = json.loads(response.text)
    emotion_response = formatted_response['emotionPredictions'][0]['emotion']
+
 
    dominant_emotion = max(emotion_response, key=emotion_response.get)
 
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+""" Main server file
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +7,21 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """ Returns text of emotion analyzer output
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     obj = emotion_detector(text_to_analyze)
-    return {"For the given statement ,the system respose is": obj, "The dominant emotion is": obj['dominant_emotion']}
+    if obj['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+    return {"For the given statement ,the system respose is": obj,
+    "The dominant emotion is": obj['dominant_emotion']}
 
 @app.route("/")
 def render_index_page():
+    """ Render index page
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+   
